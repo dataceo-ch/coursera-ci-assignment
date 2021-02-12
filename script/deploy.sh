28653ec3d215788a3637eb3cb8a5f25a3f6a84ae
Abdellah LAHOUAOUI <abdellah.lahouaoui@gmail.com>
2021-02-12 17:44:09 +0100

    fix docker login shell line

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,6 +4,6 @@ set -e
 DOCKER_REPO=coursera-ci-assignment
 
 ./gradlew -p ./monolithic/ui packageToContainer
-echo \"${DOCKER_PASSWORD}\"|docker login --username \"${DOCKER_LOGIN}\" --password-stdin
+echo ${DOCKER_PASSWORD}|docker login --username ${DOCKER_LOGIN} --password-stdin
 docker tag zutherb/monolithic-shop:latest ${DOCKER_LOGIN}/${DOCKER_REPO}:latest
 docker push ${DOCKER_LOGIN}/${DOCKER_REPO}:latest
